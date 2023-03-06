f1122e98776a82caf269719e28f51a3c2e68e1b2
tfaaguilar <e_aguilarvelez@fanshaweonline.ca>
2023-03-06 16:00:20 -0500

    Set Up

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// drum kit: press a key to play the sound. use JavaScript to retrieve the data-key attribute and then use that as a selector to find the matching audio file and play it!
+console.log('music player script file');
+
+//Listen for keyboard events happening in the browser window
+window.addEventListener('keyup' , findMatchingAudio);
+
+function findMatchingAudio(event) {
+    debugger
+}
+
+    
+
